69d7ae0cba91e847a9f6012166dcaf17657ea763
VishnuGunaseelan <vishnu.gunaseelan@q-point.com>
2022-01-09 18:23:58 +0100

    Update Index.cshtml.cs

--- Pages/Events/Index.cshtml.cs
+++ Pages/Events/Index.cshtml.cs
@@ -9,7 +9,23 @@ using Microsoft.EntityFrameworkCore;
 using DreamCreationDanceCrew.Data;
 using DreamCreationDanceCrew.Models;
 
+namespace DreamCreationDanceCrew.Pages.Events
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DreamCreationDanceCrew.Data.DreamCreationDanceCrewContext _context;
 
+        public IndexModel(DreamCreationDanceCrew.Data.DreamCreationDanceCrewContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Event> Event { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Groups { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string EventGroup { get; set; }
 
         /*
         public async Task OnGetAsync()

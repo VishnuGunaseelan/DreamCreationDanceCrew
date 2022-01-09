57c76c912b4b7cfc404c03cad29d0ab834c2b37b
VishnuGunaseelan <vishnu.gunaseelan@q-point.com>
2022-01-09 18:22:33 +0100

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,27 +3,3 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 
-namespace DreamCreationDanceCrew.Controllers
-{
-
-    public class HomeController : Controller
-    {
-        private readonly DreamCreationDanceCrew.Data.DreamCreationDanceCrewContext _context;
-        public HomeController(DreamCreationDanceCrew.Data.DreamCreationDanceCrewContext context)
-        {
-            _context = context;
-        }
-
-        public IActionResult Index()
-        {
-        Event model = new Event();
-        model.CheckboxGroups = new List<EnumModel>();
-        foreach (Groups groups in Enum.GetValues(typeof(Groups)))
-            {
-                model.CheckboxGroups.Add(new EnumModel() { Group = groups, IsSelected = false });
-            }
-        return View(model);
-        }
-
-    }
-} 
